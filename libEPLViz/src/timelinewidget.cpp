b5495fb01fd688d7d49016cfd6796155ae965a44
--global <joshua.schauer@gmx.de>
2017-02-07 16:59:08 +0100

    Removed x and y axis from timeline

--- libEPLViz/src/timelinewidget.cpp
+++ libEPLViz/src/timelinewidget.cpp
@@ -1,6 +1,20 @@
 #include "timelinewidget.hpp"
 #include "qwt_plot.h"
+#include <QEvent>
 
 TimelineWidget::TimelineWidget(QWidget *parent) : QDockWidget(parent) {
-  QwtPlot plot = (QWTPlot)this->findChild("qwtPlotTimeline");
+}
+
+bool QWidget::event(QEvent *event) {
+  // Only handling Polish events, call parent in any other case
+  if (event->type() == QEvent::Polish) {
+    // Setting options of the QwtPlot
+    QString name = "qwtPlotTimeline";
+    QwtPlot *plot = this->findChild<QwtPlot *>(name);
+    plot->enableAxis(QwtPlot::yLeft, false);
+    plot->enableAxis(QwtPlot::xBottom, false);
+  } else {
+    return QWidget::event(event);
+  }
+  return true;
 }

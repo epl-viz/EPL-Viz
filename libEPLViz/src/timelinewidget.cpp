a9d06d07a8716f22b6d9bd570457c5bc0ca2aa79
--global <joshua.schauer@gmx.de>
2017-02-07 14:08:22 +0100

    Replaced Timeline mockup with actual class

--- libEPLViz/src/timelinewidget.cpp
+++ libEPLViz/src/timelinewidget.cpp
@@ -1,3 +1,6 @@
 #include "timelinewidget.hpp"
+#include "qwt_plot.h"
 
-TimelineWidget::TimelineWidget(QWidget *parent) : QDockWidget(parent) {}
+TimelineWidget::TimelineWidget(QWidget *parent) : QDockWidget(parent) {
+  QwtPlot plot = (QWTPlot)this->findChild("qwtPlotTimeline");
+}

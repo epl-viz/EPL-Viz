fc406f01d23daf5c06c11b4c48ed2f5b0caa5dfc
Joshua Schauer <joshua.schauer@student.kit.edu>
2017-02-11 16:40:56 +0100

    Fixed removing of axis
    Fixed a segfault and added licence

--- libEPLViz/src/timelinewidget.cpp
+++ libEPLViz/src/timelinewidget.cpp
@@ -1,17 +1,50 @@
+/* Copyright (c) 2017, EPL-Vizards
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the EPL-Vizards nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL EPL-Vizards BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/*!
+ * \file timelinewidget.hpp
+ */
 #include "timelinewidget.hpp"
 #include "qwt_plot.h"
 #include <QEvent>
+#include <stdio.h>
+#include <QDebug>
 
 TimelineWidget::TimelineWidget(QWidget *parent) : QDockWidget(parent) {}
 
-bool QWidget::event(QEvent *event) {
+bool TimelineWidget::event(QEvent *event) {
   // Only handling Polish events, call parent in any other case
   if (event->type() == QEvent::Polish) {
-    // Setting options of the QwtPlot
-    QString  name = "qwtPlotTimeline";
-    QwtPlot *plot = this->findChild<QwtPlot *>(name);
-    plot->enableAxis(QwtPlot::yLeft, false);
-    plot->enableAxis(QwtPlot::xBottom, false);
+    QwtPlot *plot = this->findChild<QwtPlot *>("qwtPlotTimeline");
+    // Event gets thrown multiple times, even when not all children have been created
+    if (plot) {
+      plot->enableAxis(QwtPlot::yLeft, false);
+      plot->enableAxis(QwtPlot::xBottom, false);
+    } else {
+      qDebug() << "PlotTimeline not found, this is ok";
+    }
   } else {
     return QWidget::event(event);
   }

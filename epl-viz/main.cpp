9ca5f17e946675177aa473f03af3f2b81df61704
Joshua Schauer <joshua.schauer@student.kit.edu>
2017-03-04 19:03:57 +0100

    Fixed init of EPL_DataCollect

--- epl-viz/main.cpp
+++ epl-viz/main.cpp
@@ -1,7 +1,12 @@
+#include "Init.hpp"
 #include "mainwindow.hpp"
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
+  EPL_DataCollect::Init init;
+  if (!init.getIsOK()) {
+    return 1;
+  }
   QApplication a(argc, argv);
   MainWindow   w;
   w.show();

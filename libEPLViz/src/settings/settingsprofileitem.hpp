09a73ee624dcee11b46ac551b041edd03393aadc
Daniel Mensinger <daniel@mensinger-ka.de>
2017-03-07 01:22:08 +0100

    Improved SettingsWindow

--- libEPLViz/src/settings/settingsprofileitem.hpp
+++ libEPLViz/src/settings/settingsprofileitem.hpp
@@ -26,10 +26,28 @@
 
 #pragma once
 
+#include "CaptureInstance.hpp"
 #include <QListWidget>
+#include <unordered_map>
 
+class SettingsWindow;
 
 class SettingsProfileItem : public QListWidgetItem {
+ private:
+  struct Config {
+    EPL_DataCollect::CaptureInstance::Config backConf;
+    std::unordered_map<int, EPL_DataCollect::CaptureInstance::NodeConfig> nodes;
+    std::string pythonPluginsDir = "/opt";
+    int         currentNode;
+  };
+
+  Config  cfg;
+  QString name;
+
  public:
-  SettingsProfileItem();
+  SettingsProfileItem(QString str, QListWidget *parent);
+
+  Config  getConfig();
+  QString getName();
+  friend class SettingsWindow;
 };

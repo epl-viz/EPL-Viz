d0f93e4c8cc4a4b818730743ba16f6c0f16d6094
mensinda <daniel@mensinger-ka.de>
2017-03-25 22:09:17 +0100

    Added more AppImage support

--- epl-viz/main.cpp
+++ epl-viz/main.cpp
@@ -1,16 +1,39 @@
+#include "EPLVizDefines.hpp"
 #include "Init.hpp"
 #include "MainWindow.hpp"
 #include "PythonInit.hpp"
 #include <QApplication>
 
+using namespace EPL_DataCollect;
+using namespace EPL_DataCollect::constants;
+using namespace EPL_Viz;
+using namespace EPL_Viz::constants;
+using namespace std;
+
 int main(int argc, char *argv[]) {
-  EPL_DataCollect::Init                init;
-  EPL_DataCollect::plugins::PythonInit pyInit;
+  std::string plDir         = EPL_DC_WS_PLUGINS_DIR;
+  std::string installPrefix = EPL_DC_INSTALL_PREFIX;
+
+  if (EPL_VIZ_OVERRIDE_INSTALL_PREFIX) {
+    installPrefix = "./usr";
+    plDir.replace(0, EPL_DC_INSTALL_PREFIX.length(), installPrefix);
+  }
+
+  std::cout << plDir << std::endl;
+
+  if (argc >= 1)
+    return 1;
+
+  Init                init(plDir);
+  plugins::PythonInit pyInit;
   if (!init.getIsOK()) {
     return 1;
   }
-  QApplication        a(argc, argv);
-  EPL_Viz::MainWindow w;
+
+  pyInit.addPath(installPrefix + "/lib/eplviz");
+
+  QApplication a(argc, argv);
+  MainWindow   w;
   w.show();
 
   return a.exec();

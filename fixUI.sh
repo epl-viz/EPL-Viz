2798a9179032610bf20a8b47d3ca860e56e9b078
mensinda <daniel@mensinger-ka.de>
2017-06-16 18:23:25 +0200

    Improved fixUI.sh

--- fixUI.sh
+++ fixUI.sh
@@ -7,6 +7,7 @@ FILE_LIST=( $(find . -name "*.ui" -type f) )
 for i in "${FILE_LIST[@]}"; do
   OLD_SUM="$(sha512sum "$i")"
   sed -i ':a;N;$!ba;s/\(iconset[ ]\+theme[^>]\+\)>[\n ]*<normaloff>.<\/normaloff>.<\/iconset>/\1\/>/g' "$i"
+  perl -0777 -pi -e 's/(<property name="windowTitle">[ \t\n]*<string>[^&<]*)&amp;/\1/igs' "$i"
   if [[ "$OLD_SUM" != "$(sha512sum "$i")" ]]; then
     echo "CHANGED $i"
   fi
